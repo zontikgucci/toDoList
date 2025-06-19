85a9d06af71a6851b7d2a61fa7289477c6d7cf6b
Gubar Vlad <endlessloop@bk.ru>
2025-06-19 21:41:04 +0400

    1 задание. Вывод todo с помощью JSON Placeholder

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,46 @@
+import { useEffect, useState } from 'react';
 import styles from './App.module.css';
 
 export const App = () => {
+  const [todo, setTodos] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then((response) => response.json())
+      .then((responseTodo) => setTodos(responseTodo))
+      .catch((error) => alert(error))
+      .finally(setIsLoading(false));
+  }, []);
+
   return (
-    <div className={styles.container}>
-      <h1 className={styles.title}>To-Do List</h1>
-      <div className={styles.inputSection}>
-        <input type="text" placeholder="Add a new task..." className={styles.todoInput} />
-        <button className={styles.addButton}>Add</button>
-      </div>
-      <div className={styles.controlsSection}>
-        <input type="text" placeholder="Search tasks..." className={styles.searchInput} />
-        <button className={styles.sortButton}>Sort</button>
-      </div>
-      <ul className={styles.todoList}>
-        <li className={`${styles.todoItem} ${styles.completed}`}>
-          <span className={styles.todoText}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore consequatur vel nostrum explicabo commodi
-            nemo mollitia rem beatae dicta. Sit sunt enim vero vel recusandae dignissimos perferendis quam eveniet
-            blanditiis.
-          </span>
-          <div className={styles.actions}>
-            <button className={`${styles.actionButton} ${styles.completeButton}`}>Complete</button>
-            <button className={`${styles.actionButton} ${styles.deleteButton}`}>Delete</button>
+    <>
+      {isLoading ? (
+        <span className={styles.loading}></span>
+      ) : (
+        <div className={styles.container}>
+          <h1 className={styles.title}>To-Do List</h1>
+          {/* <div className={styles.inputSection}>
+            <input type="text" placeholder="Add a new task..." className={styles.todoInput} />
+            <button className={styles.addButton}>Add</button>
           </div>
-        </li>
-      </ul>
-    </div>
+          <div className={styles.controlsSection}>
+            <input type="text" placeholder="Search tasks..." className={styles.searchInput} />
+            <button className={styles.sortButton}>Sort</button>
+          </div> */}
+          <ul className={styles.todoList}>
+            {[todo].map(({ id, title, completed }) => (
+              <li className={`${styles.todoItem} ${completed ? styles.completed : ''}`} key={id}>
+                <span className={styles.todoText}>{title}</span>
+                <div className={styles.actions}>
+                  <button className={`${styles.actionButton} ${styles.completeButton}`}>Complete</button>
+                  <button className={`${styles.actionButton} ${styles.deleteButton}`}>Delete</button>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </>
   );
 };
